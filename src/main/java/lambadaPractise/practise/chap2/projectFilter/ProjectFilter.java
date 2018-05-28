8d9920414378068a7a82adaf9805078c018b13ad
pengrenjun <809507790@qq.com>
2018-05-28 16:52:40 +0800

    行为参数化

--- src/main/java/lambadaPractise/practise/chap2/projectFilter/ProjectFilter.java
+++
@@ -1,8 +0,0 @@
-package lambadaPractise.practise.chap2.projectFilter;
-
-import lambadaPractise.DataBase.Project;
-
-public interface ProjectFilter {
-
-    boolean test(Project project, Object arg);
-}
